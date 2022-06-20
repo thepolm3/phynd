eba04616c65185a8b41ed8a50c89cb0789c89304
thepolm3 <thepolm3@gmail.com>
2022-06-20 20:07:28 +0100

    update build log to be more helpful

--- build-img/create-thumbnails.js
+++ build-img/create-thumbnails.js
@@ -6,7 +6,7 @@ import { exit } from 'process';
 
 
 // arg 1 is min, arg 2 is max
-const from = process.argv[2] || -Infinity
+const from = process.argv[2] || 0
 const to = process.argv[3] || Infinity
 
 glob(`**/*.*`, { nocase: true, cwd: 'img' }, (er, files) => {
@@ -14,13 +14,14 @@ glob(`**/*.*`, { nocase: true, cwd: 'img' }, (er, files) => {
         console.error(er)
     }
 
+
     files.filter((_, i) => {return i <= to && i >= from})
         .forEach((filename, i) => {
         let filepath = path.parse(filename)
         let folder = filepath.name.split('.')[0]
         let full_name = `${folder}/${filepath.name}.jpg`
         if (!fs.existsSync(`static/img/full/${full_name}`)) {
-            console.log(`Converting: ${filename} ${i}/${files.length}`)
+            console.log(`Converting: ${filename} ${i+from}/${files.length}`)
             Jimp.read(`img/${filename}`, function (err, file) {
                 if (err) {
                     console.error(err)
@@ -31,11 +32,11 @@ glob(`**/*.*`, { nocase: true, cwd: 'img' }, (er, files) => {
                             .resize(Jimp.AUTO, 256)
                             .quality(60)
                             .write(`static/img/thumbs/${full_name}`)
-                        console.log(`Finished ${filename} ${i}/${files.length}`)
+                        console.log(`Finished ${filename} ${i+from}/${files.length}`)
                 }
             })
         } else {
-            console.log(`Skipped ${filename} ${i}/${files.length}`)
+            console.log(`Skipped ${filename} ${i+from}/${files.length}`)
         }
     })
 })
